f191d804d67973e992affe89dda760a842071520
Thure Waller <thure.waller@gmail.com>
2021-05-30 22:42:22 +0200

    WorkInProgressText + deploy settings

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        thurewaller
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      Work in progress
     </div>
   </div>
 </template>
