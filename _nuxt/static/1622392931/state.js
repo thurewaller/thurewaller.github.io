ba9f4f350af98bd407551a77caf6a7f3e34796ee
Thure Waller <thure.waller@gmail.com>
2021-05-30 22:41:21 +0200

    Updates

--- _nuxt/static/1622392931/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fthurewaller@github.io\u002F_nuxt\u002Fstatic\u002F1622392931",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fthurewaller@github.io\u002F",assetsPath:"\u002Fthurewaller@github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
